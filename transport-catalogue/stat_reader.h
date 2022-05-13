a7bb3b76cd57c6c3e55f75b72e152746622a783f
OrekhovaMariy <95665103+OrekhovaMariy@users.noreply.github.com>
2022-05-13 17:21:14 +0300

    итоговая работа спринт 9
    итоговая работа спринт 9

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,20 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"    
+#include "input_reader.h"           
+#include <utility>          
+#include <string>
+#include <string_view>      
+#include <sstream>          
+#include <iomanip>         
+#include <istream>          
+#include <iostream>         
+
+namespace transport_catalogue::stat_reader
+{
+    std::ostream& operator<<(std::ostream&, const Stop*);
+    std::ostream& operator<<(std::ostream& os, const Bus* route);
+
+    std::ostream& ProcessRequests(std::ostream&, TransportCatalogue&, std::istream&);   
+    std::ostream& ExecuteRequest(std::ostream&, TransportCatalogue&, RequestQuery&);    
+
+}
