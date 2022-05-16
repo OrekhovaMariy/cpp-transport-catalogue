ff08640f28e13ba77f393a2acdb604982616138b
OrekhovaMariy <95665103+OrekhovaMariy@users.noreply.github.com>
2022-05-16 22:18:02 +0300

    правки от 16.05.22 итоговая работа 9 спринт
    правки от 16.05.22

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -14,8 +14,9 @@ namespace transport_catalogue::stat_reader
     std::string PrintBus(const transport_catalogue::BusInfo& bus);
     std::string PrintStop(const transport_catalogue::StopInfo& stop);
 
-    template<typename T>
-    void Read(T& is, TransportCatalogue& tc)
+   
+    template<typename T, typename U>
+    void ProcessStat(T& is, U& os, TransportCatalogue& tc)
     {
         size_t request_num = 0;
         is >> request_num;
@@ -29,38 +30,15 @@ namespace transport_catalogue::stat_reader
             auto tmp = detail::Split(line, ' ');
             tmp.first = detail::TrimString(tmp.first);
             tmp.second = detail::TrimString(tmp.second);
-            RequestQuery query;
-            query.requery_ = tmp.second;
             if (tmp.first == "Stop"sv)
             {
-                query.outputtype_ = RequestQueryType::GetBusesForStop;
-                tc.request_data_.push_back({ query.outputtype_, query.requery_ });
-            }
-            if (tmp.first == "Bus"sv) {
-                query.outputtype_ = RequestQueryType::GetRouteByName;
-                tc.request_data_.push_back({ query.outputtype_, query.requery_ });
-            }
-        }
-    }
-
-    template<typename T>
-    void Write(T& os, TransportCatalogue& tc)
-    {
-        auto req = tc.request_data_;
-        for (const auto& data : req)
-        {
-            const auto info = data;
-            if (info.outputtype_ == RequestQueryType::GetRouteByName)
-            {
-                const auto bus_info = tc.GetBusInfo(info.requery_);
-                os << PrintBus(bus_info);
-            }
-            if (info.outputtype_ == RequestQueryType::GetBusesForStop)
-            {
-                const auto stop_info = tc.GetStopInfo(info.requery_);
+                const auto stop_info = tc.GetStopInfo(tmp.second);
                 os << PrintStop(stop_info);
             }
+            if (tmp.first == "Bus"sv) {
+                const auto bus_info = tc.GetBusInfo(tmp.second);
+                os << PrintBus(bus_info);
+            }
         }
-        req.clear();
     }
 }
