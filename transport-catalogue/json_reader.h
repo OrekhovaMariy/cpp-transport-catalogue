3b21ffdeab3043a0958c2782718a7d9cb938780f
OrekhovaMariy <95665103+OrekhovaMariy@users.noreply.github.com>
2022-05-30 22:20:49 +0300

    спринт 10 30.05.22 корректировки
    спринт 10 30.05.22 корректировки

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -7,6 +7,12 @@
 
 namespace json_pro {
 	void LoadJSON(transport_db::TransportCatalogue& transport_catalogue, std::istream& input);
-	void FillCatalog(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc);
+
+	void FillCatalogBusAndStop(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc);
+	void FillCatalogBusInfoandStopInfo(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc);
+
 	void PrintAnswer(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc, std::string result_map_render);
+	json::Dict PrintStop(transport_db::TransportCatalogue& transport_catalogue, const json::Node& node_map, int id);
+	json::Dict PrintBus(transport_db::TransportCatalogue& transport_catalogue, const json::Node& node_map, int id);
+	json::Dict PrintVisual(std::string result_map_render, int id);
 }
