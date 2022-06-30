6126599d609bbc8a5a17f52f39ff076c403c25d7
Mariya <orekhovamariy@yandex.ru>
2022-06-30 13:52:42 +0300

    спринт 12 от 30.06.22
    спринт 12 от 30.06.22

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -2,6 +2,9 @@
 
 #include "transport_catalogue.h"
 #include "json_builder.h"
+#include "router.h"
+#include "transport_router.h"
+#include "router.h"
 
 #include <iostream>
 
@@ -10,9 +13,11 @@ namespace json_pro {
 
 	void FillCatalogBusAndStop(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc);
 	void FillCatalogBusInfoandStopInfo(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc);
+	void FillCatalogGrafOfRoute(const json::Dict& doc, transport_db::TransportCatalogue& transport_catalogue, graph::DirectedWeightedGraph<double>& graph, transport_router::TransportRouter& router);
 
-	void PrintAnswer(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc, std::string result_map_render);
+	void PrintAnswer(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc, std::string result_map_render, graph::Router<double> transport_router, transport_router::TransportRouter router);
 	json::Dict PrintStop(transport_db::TransportCatalogue& transport_catalogue, const json::Node& node_map, int id);
 	json::Dict PrintBus(transport_db::TransportCatalogue& transport_catalogue, const json::Node& node_map, int id);
+	json::Dict PrintGraf(transport_db::TransportCatalogue& transport_catalogue, const json::Node& node_map, int id, graph::Router<double> transport_router, transport_router::TransportRouter router);
 	json::Dict PrintVisual(std::string result_map_render, int id);
 }
