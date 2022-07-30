5fbd8658a389df205e5dd791b57e4c406e5bdd94
Mariya <orekhovamariy@yandex.ru>
2022-07-30 08:50:36 +0300

    Итоговая работа спринт 14
    Итоговая работа спринт 14

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -5,19 +5,41 @@
 #include "router.h"
 #include "transport_router.h"
 #include "router.h"
+#include "serialization.h"
 
 #include <iostream>
 
 namespace json_pro {
-	void LoadJSON(transport_db::TransportCatalogue& transport_catalogue, std::istream& input);
+	class JSONreader {
+	private:
+		serialize::Serialization& serializator_;
+		transport_db::TransportCatalogue& t_c_;
+		std::string result_map_render_{};
+		transport_router::TransportRouter& transport_router_;
+		json::Array base_requests_{};
+		json::Dict route_requests_{};
+		json::Array stat_requests_{};
+		json::Dict render_requests_{};
 
-	void FillCatalogueStop(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc);
-	void FillCatalogueBus(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc);
-	void SetGraphInfo(const json::Dict& doc, transport_router::TransportRouter& router);
+	public:
+		explicit JSONreader(serialize::Serialization& serializator,
+			transport_db::TransportCatalogue& t_c,
+			std::string result_map_render,
+			transport_router::TransportRouter& transport_router);
 
-	void PrintAnswer(transport_db::TransportCatalogue& transport_catalogue, json::Document& doc, std::string result_map_render, graph::Router<double> transport_router, transport_router::TransportRouter router);
-	json::Dict PrintStop(transport_db::TransportCatalogue& transport_catalogue, const json::Node& node_map, int id);
-	json::Dict PrintBus(transport_db::TransportCatalogue& transport_catalogue, const json::Node& node_map, int id);
-	json::Dict PrintGraph(transport_db::TransportCatalogue& transport_catalogue, const json::Node& node_map, int id, graph::Router<double> transport_router, transport_router::TransportRouter router);
-	json::Dict PrintVisual(std::string result_map_render, int id);
+		void LoadJSON( std::istream& input);
+		void ReadRequests(std::istream& input);
+
+		void FillCatalogueStop( json::Document& doc);
+		void FillCatalogueBus( json::Document& doc);
+		void SetGraphInfo(const json::Dict& doc, transport_router::TransportRouter& router);
+
+		void PrintAnswer();
+
+		std::filesystem::path DoSerialization(const json::Dict& queryset);
+		json::Dict PrintStop( const json::Node& node_map, int id);
+		json::Dict PrintBus( const json::Node& node_map, int id);
+		json::Dict PrintGraph( const json::Node& node_map, int id, graph::Router<double> transport_router, transport_router::TransportRouter router);
+		json::Dict PrintVisual(std::string result_map_render, int id);
+	};
 }
