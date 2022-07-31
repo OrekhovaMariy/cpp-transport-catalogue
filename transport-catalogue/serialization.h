1c0bbe98806cdaf0fac8916577c5448875126278
Mariya <orekhovamariy@yandex.ru>
2022-07-31 08:43:52 +0300

    спринт 14 внесение корректировок
    спринт 14 внесение корректировок

--- transport-catalogue/serialization.h
+++ transport-catalogue/serialization.h
@@ -19,33 +19,33 @@ namespace serialize {
 		void SetSetting(const std::filesystem::path& path_to_base);
 		void Serialize(transport_db::TransportCatalogue& cat);
 		void DeserializeCatalogue(transport_db::TransportCatalogue& cat);
-		std::string InputMapRenderer();
-		void SetMapRender(std::string map);
-		void OutputRouterSetVelosity(std::map <std::string, double> router_settings_velosity);
-		void OutputRouterSetTime(std::map <std::string, int> router_settings_time);
-		double InputRouterSetVelosity();
-		int InputRouterSetTime();
+		std::string DeserializeMapRenderer();
+		void SerializeMapRender(std::string map);
+		void SerializeRouterSetVelosity(std::map <std::string, double> router_settings_velosity);
+		void SerializeRouterSetTime(std::map <std::string, int> router_settings_time);
+		double DeserializeRouterSetVelosity();
+		int DeserializeRouterSetTime();
 
 	private:
         proto_catalogue::Stop SaveStop(const domain::Stop& stop) const;
 		proto_catalogue::DistanceFromTo SaveDistanceFromTo(domain::Stop* from, domain::Stop* to, size_t dist) const;
 		proto_catalogue::Bus SaveBus(const domain::Bus& bus) const;
 
-        void OutputStops();
+        void SerializeStops();
 
-		void OutputDistanceFromTo();
+		void SerializeDistanceFromTo();
 
-        void OutputBuses();
+        void SerializeBuses();
 
         void LoadStop(const proto_catalogue::Stop& stop);
 		void LoadDistanceFromTo(const proto_catalogue::DistanceFromTo& distance_from_to);
     void LoadBus(const proto_catalogue::Bus& bus);
 
-    void InputStops();
+    void DeserializeStops();
 
-	void InputDistanceFromTo();
+	void DeserializeDistanceFromTo();
 
-	void InputBuses();
+	void DeserializeBuses();
 
 		std::filesystem::path path_;
 		transport_db::TransportCatalogue& transport_catalogue_;
@@ -53,3 +53,6 @@ namespace serialize {
 	};
 
 }
+
+
+
